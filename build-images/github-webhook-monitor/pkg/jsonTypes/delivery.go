e7acab485c60e56c550397dbfd18e63a228a533f
James Davies <jdavies47@hotmail.co.uk>
2022-10-28 10:38:51 +0100

    init push of github monitor
    Signed-off-by: James Davies <jdavies47@hotmail.co.uk>

---
+++ build-images/github-webhook-monitor/pkg/jsonTypes/delivery.go
@@ -0,0 +1,12 @@
+package jsontypes
+
+type WebhookRequest struct {
+	Id      int     `json:"id"`
+	Event   string  `json:"event"`
+	Request Request `json:"request"`
+}
+
+type Request struct {
+	Headers map[string]string `json:"headers"`
+	Payload interface{}       `json:"payload"`
+}
