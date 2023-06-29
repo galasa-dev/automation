fc181b7356d747061071bc2e2263637f30b9e9fc
Mike Cobbett <77053+techcobweb@users.noreply.github.com>
2023-06-29 13:37:56 +0100

    create-argocd-apps.sh script asks for the release process type
    Signed-off-by: Mike Cobbett <77053+techcobweb@users.noreply.github.com>

--- releasePipeline/02-create-argocd-apps.sh
+++ releasePipeline/02-create-argocd-apps.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 
+
+
+
+
+PS3="Select the type of release process please: "
+
+select lng in release pre-release
+do
+    case $lng in
+        "release")
+            release_type="release"
+            break
+            ;;
+        "pre-release")
+            release_type="prerelease"
+            break
+            ;;
+        *)
+           echo "Unrecognised input.";;
+    esac
+done
+
+echo "Chosen type of release process: ${release_type}"
+
 set -e
                   
-argocd app create release-maven-repos \
+argocd app create ${release_type}-maven-repos \
                   --project default \
                   --sync-policy auto \
                   --sync-option Prune=true \
@@ -11,50 +35,50 @@ argocd app create release-maven-repos \
                   --path infrastructure/ibmcloud-galasadev-cluster/galasa-development/branch-maven-repository \
                   --dest-server https://kubernetes.default.svc \
                   --dest-namespace galasa-development \
-                  --helm-set wrapping.branch=release \
-                  --helm-set wrapping.imageTag=release \
+                  --helm-set wrapping.branch=${release_type} \
+                  --helm-set wrapping.imageTag=${release_type} \
                   --helm-set wrapping.deploy=true \
-                  --helm-set gradle.branch=release \
-                  --helm-set gradle.imageTag=release \
+                  --helm-set gradle.branch=${release_type} \
+                  --helm-set gradle.imageTag=${release_type} \
                   --helm-set gradle.deploy=true \
-                  --helm-set maven.branch=release \
-                  --helm-set maven.imageTag=release \
+                  --helm-set maven.branch=${release_type} \
+                  --helm-set maven.imageTag=${release_type} \
                   --helm-set maven.deploy=true \
-                  --helm-set framework.branch=release \
-                  --helm-set framework.imageTag=release \
+                  --helm-set framework.branch=${release_type} \
+                  --helm-set framework.imageTag=${release_type} \
                   --helm-set framework.deploy=true \
-                  --helm-set extensions.branch=release \
-                  --helm-set extensions.imageTag=release \
+                  --helm-set extensions.branch=${release_type} \
+                  --helm-set extensions.imageTag=${release_type} \
                   --helm-set extensions.deploy=true \
-                  --helm-set managers.branch=release \
-                  --helm-set managers.imageTag=release \
+                  --helm-set managers.branch=${release_type} \
+                  --helm-set managers.imageTag=${release_type} \
                   --helm-set managers.deploy=true \
-                  --helm-set obr.branch=release \
-                  --helm-set obr.imageTag=release \
+                  --helm-set obr.branch=${release_type} \
+                  --helm-set obr.imageTag=${release_type} \
                   --helm-set obr.deploy=true \
-                  --helm-set javadoc.branch=release \
-                  --helm-set javadoc.imageTag=release \
+                  --helm-set javadoc.branch=${release_type} \
+                  --helm-set javadoc.imageTag=${release_type} \
                   --helm-set javadoc.deploy=true \
-                  --helm-set javadocsite.branch=release \
-                  --helm-set javadocsite.imageTag=release \
+                  --helm-set javadocsite.branch=${release_type} \
+                  --helm-set javadocsite.imageTag=${release_type} \
                   --helm-set javadocsite.deploy=true \
-                  --helm-set restApiDocSite.branch=release \
-                  --helm-set restApiDocSite.imageTag=release \
+                  --helm-set restApiDocSite.branch=${release_type} \
+                  --helm-set restApiDocSite.imageTag=${release_type} \
                   --helm-set restApiDocSite.deploy=true \
-                  --helm-set eclipse.branch=release \
-                  --helm-set eclipse.imageTag=release \
+                  --helm-set eclipse.branch=${release_type} \
+                  --helm-set eclipse.imageTag=${release_type} \
                   --helm-set eclipse.deploy=true \
-                  --helm-set p2.branch=release \
-                  --helm-set p2.imageTag=release \
+                  --helm-set p2.branch=${release_type} \
+                  --helm-set p2.imageTag=${release_type} \
                   --helm-set p2.deploy=true \
-                  --helm-set isolated.branch=release \
-                  --helm-set isolated.imageTag=release \
+                  --helm-set isolated.branch=${release_type} \
+                  --helm-set isolated.imageTag=${release_type} \
                   --helm-set isolated.deploy=true \
-                  --helm-set mvp.branch=release \
-                  --helm-set mvp.imageTag=release \
+                  --helm-set mvp.branch=${release_type} \
+                  --helm-set mvp.imageTag=${release_type} \
                   --helm-set mvp.deploy=true 
 
-argocd app create release-cli \
+argocd app create ${release_type}-cli \
                   --project default \
                   --sync-policy auto \
                   --sync-option Prune=true \
@@ -64,5 +88,5 @@ argocd app create release-cli \
                   --path infrastructure/ibmcloud-galasadev-cluster/galasa-development/cli \
                   --dest-server https://kubernetes.default.svc \
                   --dest-namespace galasa-development \
-                  --helm-set branch=release \
-                  --helm-set imageTag=release                   
+                  --helm-set branch=${release_type} \
+                  --helm-set imageTag=${release_type}                   
