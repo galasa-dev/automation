6f2e1e7c0a28f11454a0125da2748d426c6fc19d
Fiona Ampofo <64271621+Akyiaa@users.noreply.github.com>
2023-07-17 17:44:03 +0100

    fiona-copyrighter-tool
    Signed-off-by: Fiona Ampofo <64271621+Akyiaa@users.noreply.github.com>

---
+++ offline-tools/copyrighter/pkg/cmd/galasacopyrighter_test.go
@@ -0,0 +1,192 @@
+/*
+ * Copyright contributors to the Galasa project
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
+package cmd
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCanAddCopyrightWhenNoneExistsAlready(t *testing.T) {
+	var input = `package mypackage
+	class AClass {
+		// Does nothing.
+	}
+	`
+
+	output, err := setCopyright(input)
+
+	assert.NotNil(t, output)
+	assert.Nil(t, err)
+
+	if !strings.HasPrefix(output, `/*
+ * Copyright contributors to the Galasa project
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */`) {
+		assert.Fail(t, "Copyright statement is not at the start of the output. output:\n%s", output)
+	}
+}
+
+func TestCopyrightAddedToOriginalContent(t *testing.T) {
+	// Given..
+	var input = `package mypackage
+	class AClass {
+		// Does nothing.
+	}
+	`
+
+	// When...
+	output, err := setCopyright(input)
+
+	// Then..
+	assert.Nil(t, err)
+	assert.NotNil(t, output)
+
+	packageLineLocation := strings.Index(output, "package mypackage")
+	assert.Greater(t, packageLineLocation, 0, "Original file content should be returned with the copyright.")
+}
+
+func TestCopyrightWhereNoneExistsAddedToStartOfFile(t *testing.T) {
+	// Given..
+	var input = `/*
+ * Copyright contributors to the Galasa project
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package mypackage
+class AClass {
+	// Does nothing.
+}
+`
+
+	// When...
+	output, err := setCopyright(input)
+
+	// Then..
+	assert.NotNil(t, output)
+	assert.Nil(t, err)
+
+	contributorLineCount := strings.Count(output, "Copyright contributors to the Galasa project")
+	assert.Equal(t, contributorLineCount, 1, "Contributor line has been repeated.")
+
+	licenseLineCount := strings.Count(output, "SPDX-License-Identifier: EPL-2.0\n")
+	assert.Equal(t, licenseLineCount, 1, "License line has been repeated.")
+}
+
+func TestCopyrightNotAddedWhenPresentAlready(t *testing.T) {
+	// Given..
+	var input = `package mypackage
+	class AClass {
+		// Does nothing.
+	}
+	`
+
+	// When...
+	output, err := setCopyright(input)
+
+	// Then..
+	assert.Nil(t, err)
+	assert.NotNil(t, output)
+}
+
+func TestCanStripOutFirstCommentAndTrailingWhiteSpace(t *testing.T) {
+	// Given..
+	var input = `/*
+ * Copyright contributors to the Galasa project
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */       ` + "\t" + "\n" + "package mypackage"
+
+	// When...
+	output, err := stripOutExistingCopyright(input)
+
+	// Then..
+	assert.Nil(t, err)
+	assert.NotNil(t, output)
+	assert.Equal(t, output, "package mypackage")
+}
+
+func TestCanStripOutFirstCommentWhenThereIsNoComment(t *testing.T) {
+	// Given..
+	var input = `package mypackage`
+
+	// When...
+	output, err := stripOutExistingCopyright(input)
+
+	// Then..
+	assert.Nil(t, err)
+	assert.NotNil(t, output)
+	assert.Equal(t, "package mypackage", output)
+}
+
+func TestCanStripOutFirstCommentMostCommonExistingCopyrightStatement(t *testing.T) {
+	// Given..
+	var input = `/*
+ * Copyright contributors to the Galasa project
+ */
+ package mypackage`
+
+	// When...
+	output, err := stripOutExistingCopyright(input)
+
+	// Then..
+	assert.Nil(t, err)
+	assert.NotNil(t, output)
+	assert.Equal(t, output, "package mypackage")
+}
+
+func TestCanStripOutFirstCommentLeadingTextShouldBePreserved(t *testing.T) {
+	// Given..
+	var input = `leading text here
+/*
+ * Copyright contributors to the Galasa project
+ */
+ package mypackage`
+
+	// When...
+	output, err := stripOutExistingCopyright(input)
+
+	// Then..
+	assert.Nil(t, err)
+	assert.NotNil(t, output)
+	assert.Equal(t, output, "leading text here\npackage mypackage")
+}
+
+func TestStrippingOutFirstCommentWithNoClosingTagFailsWithError(t *testing.T) {
+	// Given..
+	var input = `
+/*
+ * Copyright contributors to the Galasa project
+ 
+ package mypackage`
+
+	// When...
+	_, err := stripOutExistingCopyright(input)
+
+	// Then..
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "comment not closed.")
+}
+
+func TestClosingCommentIsBeforeOpeningCommentFailsWithError(t *testing.T) {
+	// Given..
+	var input = `
+*/
+ * Copyright contributors to the Galasa project
+ /*
+ package mypackage`
+
+	// When...
+	_, err := stripOutExistingCopyright(input)
+
+	// Then..
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "closing comment marker found before the starting comment marker")
+}
