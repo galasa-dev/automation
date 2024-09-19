07b1ce794065c335cb5c1b8ec3cf563f861cc707
Jade Carino <carino_jade@yahoo.co.uk>
2024-09-19 10:51:15 +0100

    Create and delete simplatform app in pre and release
    Signed-off-by: Jade Carino <carino_jade@yahoo.co.uk>

--- releasePipeline/92-delete-argocd-apps.sh
+++ releasePipeline/92-delete-argocd-apps.sh
@@ -8,7 +8,9 @@
 
 argocd app delete release-maven-repos
 argocd app delete release-cli
+argocd app delete release-simplatform
 argocd app delete prerelease-maven-repos
 argocd app delete prerelease-cli
+argocd app delete prerelease-simplatform
 
 echo "Complete"
